088b45ad9f9ce82cc7a36879b3602fa0d25e4300
Lakshman-11 <lakshmankunwar2057@gmail.com>
2022-02-16 22:55:04 +0545

    Dart_newbie

---
+++ Flutter/lib/main.dart
@@ -0,0 +1,104 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  List<Offset> points = <Offset>[];
+
+  @override
+  Widget build(BuildContext context) {
+    final Container sketchArea = Container(
+      margin: const EdgeInsets.all(1.0),
+      alignment: Alignment.topLeft,
+      color: Colors.blueGrey[50],
+      child: CustomPaint(
+        painter: Sketcher(points),
+      ),
+    );
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Sketcher'),
+      ),
+      body: GestureDetector(
+        onPanUpdate: (DragUpdateDetails details) {
+          setState(() {
+            RenderBox box = context.findRenderObject() as RenderBox;
+            Offset point = box.globalToLocal(details.globalPosition);
+            point = point.translate(0.0, -(AppBar().preferredSize.height));
+
+            points = List.from(points)..add(point);
+          });
+        },
+        onPanEnd: (DragEndDetails details) {
+          points.add(Offset.zero);
+        },
+        child: sketchArea,
+      ),
+      floatingActionButton: Row(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: <Widget>[
+          FloatingActionButton(
+            tooltip: 'clear Screen',
+            backgroundColor: Colors.red,
+            child: const Icon(Icons.refresh),
+            onPressed: () {
+              setState(() => points.clear());
+            },
+          ),
+          ElevatedButton(
+            onPressed: () => {},
+            child: Text('Submit'),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class Sketcher extends CustomPainter {
+  final List<Offset> points;
+
+  Sketcher(this.points);
+
+  @override
+  bool shouldRepaint(Sketcher oldDelegate) {
+    return oldDelegate.points != points;
+  }
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    Paint paint = Paint()
+      ..color = Colors.black
+      ..strokeCap = StrokeCap.round
+      ..strokeWidth = 4.0;
+
+    for (int i = 0; i < points.length - 1; i++) {
+      if (points[i] != Offset.zero && points[i + 1] != Offset.zero) {
+        canvas.drawLine(points[i], points[i + 1], paint);
+      }
+    }
+  }
+}
